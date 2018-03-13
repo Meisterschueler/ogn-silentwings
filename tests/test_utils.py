a238eceeda12b3f95046331f688dd11bd19ac89b
Konstantin Gründger <kammermark@gmx.de>
2018-03-13 20:32:25 +0100

    Fixed test_utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,7 @@
 import unittest
 
+from datetime import date
+
 from app import create_app, db
 from app.utils import process_beacon
 from app.model import Beacon
@@ -21,7 +23,7 @@ class TestDB(unittest.TestCase):
     def test(self):
         # Create beacon from raw aprs message.
         raw_flarm_message = "FLRDF0AAD>APRS,qAS,YBSS:/065751h3743.64S/14425.51E'146/004/A=000492 !W62! id0ADF0AAD -078fpm +2.3rot 10.8dB 0e -7.7kHz gps1x2"
-        flarm_message = process_beacon(raw_flarm_message)
+        flarm_message = process_beacon(raw_flarm_message, reference_date=date(2018, 3, 11))
         flarm_beacon = Beacon(**flarm_message)
 
         # Put the beacon into the db
