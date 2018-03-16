b8bf25e486fd7e1ec52da3630ea4ef29ab67d322
dspreitz <dominic@spreitz.de>
2018-03-16 22:29:13 +0100

    StrePla API Info

---
+++ app/strepla.py
@@ -0,0 +1,16 @@
+"""
+ogn@data-ogn:~/ogn-silentwings/app$ sudo netcat -l 80
+[sudo] password for ogn: 
+POST /ws/desktopStrePla.aspx HTTP/1.1
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+User-Agent: HTTP Retriever 1.0
+Host: data-ogn
+Content-Length: 54
+Cache-Control: no-cache
+
+cmd=getComp&cToken=guest&uName=guest&uPwd=guest&data=&ogn@data-ogn:~/ogn-silentwings/app$ 
+
+StrePla Server:http://strepla.de/scs/
+Port: 80
+"""
