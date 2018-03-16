34434763d8b7ffb8e59bc088ece621c8e5203f43
dspreitz <dominic@spreitz.de>
2018-03-16 22:46:55 +0100

    More scoring StrePla API infos

--- app/strepla.py
+++ app/strepla.py
@@ -13,4 +13,31 @@ cmd=getComp&cToken=guest&uName=guest&uPwd=guest&data=&ogn@data-ogn:~/ogn-silentw
 
 StrePla Server:http://strepla.de/scs/
 Port: 80
+
+---- Anfrage StrePla ----
+(mB.E]*D@[IPJI)P_POST /scs//ws/desktopStrePla.aspx HTTP/1.1
+Accept: */*
+Content-Type: application/x-www-form-urlencoded
+User-Agent: HTTP Retriever 1.0
+Host: strepla.de
+Content-Length: 54
+Cache-Control: no-cache
+Cookie: ASP.NET_SessionId=ktxbfqcj0mz0lgymiuzoh51a
+
+cmd=getComp&cToken=guest&uName=guest&uPwd=guest&data=&
+
+---- Antwort StrePla ----
+B.(mEn@t~7[IPI)JPHTTP/1.1 200 OK
+Cache-Control: public,must-revalidate
+Pragma: public
+Content-Type: text/xml; charset=utf-8
+Expires: 0
+Server: Microsoft-IIS/8.5
+X-AspNet-Version: 4.0.30319
+X-Powered-By: ASP.NET
+Date: Fri, 16 Mar 2018 21:34:21 GMT
+Content-Length: 101
+
+<?xml version="1.0"?><scs><error>Wettbewerb mit dem Schluessel guest ist nicht bekannt.</error></scs>
+
 """
