21a2e0da3be3c6080ca271b51b61c89e92ea0118
Konstantin Gründger <kammermark@gmx.de>
2018-03-17 09:37:56 +0100

    Implement read log file

--- app/utils.py
+++ app/utils.py
@@ -24,3 +24,17 @@ def process_beacon(raw_message, reference_date=None):
         else:
             subset_message = {k: message[k] for k in message.keys() & {'name', 'address', 'timestamp', 'latitude', 'longitude', 'altitude', 'track', 'ground_speed', 'climb_rate', 'turn_rate'}}
             return subset_message
+
+
+def open_file(filename):
+    """Opens a regular or unzipped textfile for reading."""
+    import gzip
+    f = open(filename, 'rb')
+    a = f.read(2)
+    f.close()
+    if (a == b'\x1f\x8b'):
+        f = gzip.open(filename, 'rt')
+        return f
+    else:
+        f = open(filename, 'rt')
+        return f
