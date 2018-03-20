f9aa0e89671da8387c6ea521bb63f59fb2ce53a6
dspreitz <dominic@spreitz.de>
2018-03-20 20:17:28 +0100

    Initial info about StrePla API

---
+++ app/strepla.py
@@ -0,0 +1,7 @@
+# flake8: noqa
+"""
+Initiale Anfrage:
+http://strepla.de/scs/ws/desktopStrePla.aspx?cmd=getComp&cToken=987060a472d9665b8198a3ce6d50ae43&uName=Spreitz-Test&uPwd=ogn-silentwings&data=&
+http://strepla.de/scs/ws/desktopStrePla.aspx?cmd=getTaskCompDays&cToken=987060a472d9665b8198a3ce6d50ae43&uName=Spreitz-Test&uPwd=ogn-silentwings&data=<params><class id="1057"/></params>&
+
+
