2bb0ab36f7a01ad708b85b2d35330e9494f7f928
dspreitz <dominic@spreitz.de>
2018-04-05 10:34:57 +0200

    Added allocation of StrePla data to ogn-siltenwings data structure

--- app/strepla.py
+++ app/strepla.py
@@ -1,19 +1,76 @@
 # flake8: noqa
 import urllib.request, json
+from datetime import datetime
+from app.model import Contest, ContestClass, Contestant, Pilot, Task, Location
 
 def get_strepla_contests_info():
     i = 0
     with urllib.request.urlopen("https://www.strepla.de/scs/ws/competition.ashx?cmd=recent&daysPeriod=360") as url:
         data = json.loads(url.read().decode())
+        print("\nID : Name of competition - Place of competition")
+        print("=================================================================")
         while i < len(data):
             data_dict = data[i]
             print(str(data_dict['id']) + ": " + str(data_dict['name']) + " - " + str(data_dict['Location']))
             i += 1
 
 def get_strepla_contest(cID):
+    contests = list()
+    # Process contest location and date info
     with urllib.request.urlopen("https://www.strepla.de/scs/ws/competition.ashx?cmd=info&cId=" + str(cID) + "&daysPeriod=360") as url:
-        data = json.loads(url.read().decode())[0]
-        print(data)
+        contest_data = json.loads(url.read().decode())[0]
+        # print(contest_data)
+        contest_dict = {'end_date': datetime.strptime(contest_data['lastDay'], "%Y-%m-%dT%H:%M:%S"),
+                        'name': contest_data['name'],
+                        'start_date': datetime.strptime(contest_data['firstDay'], "%Y-%m-%dT%H:%M:%S")}
+        contest = Contest(**contest_dict)
+
+        location_dict = {'name': contest_data['Location']}
+        location = Location(**location_dict)
+        contest.location = location
+
+        # Process contest class info
+        with urllib.request.urlopen("https://www.strepla.de/scs/ws/compclass.ashx?cmd=overview&cID=" + str(cID)) as url:
+            class_data = json.loads(url.read().decode())
+            for contest_class_row in class_data:
+                print("contest_class_row:\n", contest_class_row)
+                contest_class_dict = {'category': contest_class_row['name'],
+                                  'type': contest_class_row['rulename']}
+
+                contest_class = ContestClass(**contest_class_dict)
+                contest_class.contest = contest
+
+                # print(contest_class_row['name'])
+                # Process pilots of class
+                # print("https://www.strepla.de/scs/ws/pilot.ashx?cmd=competitors&cId=" + str(cID) + "&cc=" + str(contest_class_row['name']))
+                with urllib.request.urlopen("https://www.strepla.de/scs/ws/pilot.ashx?cmd=competitors&cId=" + str(cID) + "&cc=" + str(contest_class_row['name'])) as url:
+                    pilot_data = json.loads(url.read().decode())
+                    # print(pilot_data) 
+                    if (len(pilot_data) == 0):
+                        print("Class name not recognized. Aborting")
+                        return
+
+                    for pilot_row in pilot_data:
+                        # print(str(pilot_row['glider_callsign']) + ": " + str(pilot_row['logger1']) + " - " + str(pilot_row['name']))
+                        # print(pilot_row)
+                        contestant_dict = {'aircraft_model': pilot_row['glider_name'],
+                                           'aircraft_registration' : pilot_row['glider_callsign'],
+                                           'contestant_number': pilot_row['glider_cid'],
+                                           'handicap': pilot_row['glider_index'],
+                                           'live_track_id': pilot_row['flarm_ID']}
+                        contestant = Contestant(**contestant_dict)
+                        contestant.contest_class = contest_class
+
+                        pilot_dict = {'first_name': pilot_row['name'].rsplit(',',1),
+                                      'last_name': pilot_row['name'].split(',',1),
+                                      'nationality': pilot_row['country']}
+                        pilot = Pilot(**pilot_dict)
+                        pilot.contestant = contestant
+
+        contests.append(contest)
+
+    return contests
+
 
 # Get classes for a specific contest
 # https://www.strepla.de/scs/ws/compclass.ashx?cmd=overview&cID=403
