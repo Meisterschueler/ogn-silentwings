633aadbaf0728cb39776a33f5133c992abf0c213
Konstantin Gründger <kammermark@gmx.de>
2018-04-06 08:54:16 +0200

    Refactoring: instead of "xxx_dict" use "parameters"

--- app/glidertracker.py
+++ app/glidertracker.py
@@ -1,4 +1,4 @@
-from app.model import Contest, Contestant
+from app.model import Contest
 from app import db
 
 
@@ -24,14 +24,14 @@ def glidertracker_filter(contest_name_with_class_type):
                 # print("This is our class: " + contest_class_type)
 
                 for contestant in contest_class.contestants:
-                    entry_dict = {'live_track_id': contestant.live_track_id,
+                    parameters = {'live_track_id': contestant.live_track_id,
                                   'aircraft_registration': contestant.aircraft_registration,
                                   'contestant_number': contestant.contestant_number,
                                   'aircraft_model': contestant.aircraft_model}
 
-                    entry = '"{live_track_id}","{aircraft_registration}","{contestant_number}","{aircraft_model}"'.format(**entry_dict)
+                    entry = '"{live_track_id}","{aircraft_registration}","{contestant_number}","{aircraft_model}"'.format(**parameters)
                     result_list.append(entry)
- 
+
     print("\n".join(result_list).replace('"', ""))
     return "\n".join(result_list)
 
